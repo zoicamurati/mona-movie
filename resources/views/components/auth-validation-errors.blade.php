9c9553dda77daeed2ed141cb41ce809531adf7b3
zoicamurati <zoicamurati19@gmail.com>
2026-05-17 21:40:18 +0200

    fixes

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,11 +2,11 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
+        <div class="font-medium text-red-600 text-center">
             {{ __('Whoops! Something went wrong.') }}
         </div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+        <ul class="mt-3 list-none text-sm text-red-600 text-center" style="list-style:none;padding:0;">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
